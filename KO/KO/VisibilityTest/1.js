d42dc0713a16d4c4cdfecd8b373a00bd097161da
zhangming870 <zhangming870@hotmail.com>
2015-11-10 14:59:42 -0500

    visibility test

---
+++ KO/KO/VisibilityTest/1.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="C:\Users\mzhang\Source\Repos\Knockout\KO\KO\Scripts/knockout-3.3.0.debug.js" />
+
+var myViewModel = {
+    canSee: ko.observable(true),
+    myFunction: function (data, event) {
+
+        this.canSee(false);
+
+        if (event.shiftKey) {
+            //do something different when user has shift key down
+        } else {
+            //do normal action
+        }
+    }
+};
+
+ko.applyBindings(myViewModel);
+
