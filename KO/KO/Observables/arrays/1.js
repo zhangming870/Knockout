8d0f99c829cd0e4082d1e71bac12d7a067169fa0
zhangming870 <zhangming870@hotmail.com>
2015-11-10 14:20:42 -0500

    second checkin

--- KO/KO/Observables/arrays/1.js
+++ KO/KO/Observables/arrays/1.js
@@ -1,4 +1,15 @@
 ﻿/// <reference path="C:\Users\mzhang\Source\Repos\Knockout\KO\KO\Scripts/knockout-3.3.0.debug.js" />
 
+var SimpleListModel = function (items) {
+    this.items = ko.observableArray(items);
+    this.itemToAdd = ko.observable("");
+    this.addItem = function () {
+        if (this.itemToAdd() != "") {
+            this.items.push(this.itemToAdd()); // Adds the item. Writing to the "items" observableArray causes any associated UI to update.
+            this.itemToAdd(""); // Clears the text box, because it's bound to the "itemToAdd" observable
+        }
+    }.bind(this);  // Ensure that "this" is always this view model
+};
 
+ko.applyBindings(new SimpleListModel(["Alpha", "Beta", "Gamma"]));
 
