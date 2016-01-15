9b0742e18767b39a4f03842e63f92a46d392cb61
zhangming870 <zhangming870@hotmail.com>
2016-01-15 14:47:54 -0500

    dropdown

---
+++ KO/KO/OptionsBinding/JavaScript1.js
@@ -0,0 +1,23 @@
+﻿// Constructor for an object with two properties
+var Country = function (name, population) {
+    this.countryName = name;
+    this.countryPopulation = population;
+};
+
+var Countrys = ko.observableArray([
+        new Country("UK", 65000000),
+        new Country("USA", 320000000),
+        new Country("Sweden", 29000000)
+]);
+
+var viewModel = {
+    availableCountries: Countrys,
+    selectedCountry: ko.observable() // Nothing selected by default
+};
+
+
+
+ko.applyBindings(viewModel);
+
+/*pre selected values*/
+viewModel.selectedCountry('UK');
